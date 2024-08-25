9d19d2d15ff8e44d9fc06e588317faba0b3c9bbf
nhoangthanh <nguyenhoangthanh2405@gmail.com>
2024-08-25 17:11:37 +0700

    update url

--- src/app/_services/stock.service.ts
+++ src/app/_services/stock.service.ts
@@ -10,8 +10,8 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class StockService {
-  private apiStockUrl = 'http://localhost:8080/api/stock'; // Địa chỉ backend của bạn
-  private apiUrl = 'http://localhost:8080/api'; // Địa chỉ backend của bạn
+  private apiStockUrl = 'https://stockbe-production.up.railway.app/api/stock'; // Địa chỉ backend của bạn
+  private apiUrl = 'https://stockbe-production.up.railway.app/api'; // Địa chỉ backend của bạn
 
   constructor(private http: HttpClient) { }
 
